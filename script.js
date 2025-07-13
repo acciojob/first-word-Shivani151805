fa2f64d4b85f50aab08b8c65d10cbfe4ba0c2864
Shivani151805 <sumaninfo97@gmail.com>
2025-07-13 20:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	const index = s.indexOf(' ');
+  if (index === -1) {
+    return s; // No space found or empty string
+  }
+  return s.substring(0, index);
 }
 
 // Do not change the code below
